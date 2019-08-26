77e1ec2828f315bbf7bfb7f4b71db133623cfef9
Ahmed Abdelhay <ahmed.abdelhay@andela.com>
2019-08-26 18:20:42 +0200

    updates and add client service as react app

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,16 +1,16 @@
-# services/users/project/__init__.py
-
-
 import os
 
-from flask import Flask  # new
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_debugtoolbar import DebugToolbarExtension
+from flask_cors import CORS  # new
 
-# instantiate the db
+# instantiate the extensions
 db = SQLAlchemy()
+toolbar = DebugToolbarExtension()
+cors = CORS()  # new
 
 
-# new
 def create_app(script_info=None):
     # instantiate the app
     app = Flask(__name__)
@@ -21,6 +21,8 @@ def create_app(script_info=None):
 
     # set up extensions
     db.init_app(app)
+    toolbar.init_app(app)
+    cors.init_app(app)  # new
 
     # register blueprints
     from project.api.users import users_blueprint
