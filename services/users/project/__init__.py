b4814f7b1354516e88a9c7bc8d61b4e5e2e4e393
Ahmed Abdelhay <ahmed.abdelhay@andela.com>
2019-08-26 16:51:12 +0200

    preparing app to ci

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -1,21 +1,34 @@
-from flask import Flask, jsonify
-from flask_restful import Resource, Api
-
-# instantiate the app
-app = Flask(__name__)
-
-api = Api(app)
-
-# set config
-app.config.from_object('project.config.DevelopmentConfig')  # new
+# services/users/project/__init__.py
 
 
-class UsersPing(Resource):
-    def get(self):
-        return {
-            'status': 'success',
-            'message': 'pong!'
-        }
+import os
+
+from flask import Flask  # new
+from flask_sqlalchemy import SQLAlchemy
+
+# instantiate the db
+db = SQLAlchemy()
 
 
-api.add_resource(UsersPing, '/users/ping')
+# new
+def create_app(script_info=None):
+    # instantiate the app
+    app = Flask(__name__)
+
+    # set config
+    app_settings = os.getenv('APP_SETTINGS')
+    app.config.from_object(app_settings)
+
+    # set up extensions
+    db.init_app(app)
+
+    # register blueprints
+    from project.api.users import users_blueprint
+    app.register_blueprint(users_blueprint)
+
+    # shell context for flask cli
+    @app.shell_context_processor
+    def ctx():
+        return {'app': app, 'db': db}
+
+    return app
